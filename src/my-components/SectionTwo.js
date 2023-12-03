f2f33ebeefb932be8f88756c73d9d7a2a3234a3b
IsaacWinner18 <149059161+IsaacWinner18@users.noreply.github.com>
2023-12-02 16:34:58 -0800

    Deployed website

--- src/my-components/SectionTwo.js
+++ src/my-components/SectionTwo.js
@@ -114,7 +114,7 @@ const SectionTwo = () => {
       // Create a downloadable link
       const downloadLink = document.createElement("a");
       downloadLink.href = URL.createObjectURL(blob);
-      downloadLink.download = "generated_audio.mp3";
+      downloadLink.download = "Viocolabs_generated_audio.mp3";
 
       // Trigger a click on the link to initiate the download
       downloadLink.click();
@@ -136,12 +136,12 @@ const SectionTwo = () => {
 
   return (
     <div>
-      <div className=" sect-cover grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 mx-4 sm:mx-2 md:mx-2 mt-2 px-4 sm:px-6 md:px-7 lg:px-7 py-7 md:pt-24 lg:pt-32  bg-white">
-        <div className="flex justify-start flex-col sm:mr-0 md:mr-2 lg:mr-10 pr-10 lg:ml-20 ml-6 ">
-          <div className="border-2 border-solid border-gray-200  font-custom opacity-90 mx-5 rounded-full px-3 sm:px-6 md:px-6 lg:px-10">
+      <div className=" sect-cover grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 mx-2 sm:mx-2 md:mx-2 mt-2 px-4 sm:px-6 md:px-7 lg:px-7 py-5 md:pt-24 lg:pt-32  bg-white">
+        <div className="flex justify-start flex-col sm:mr-0 md:mr-2 lg:mr-10 pr-10 lg:ml-20 ml-3 ">
+          <div className="border-2 border-solid border-gray-200  font-custom opacity-90 mx-3 rounded-full px-3 sm:px-6 md:px-6 lg:px-10 text-xs">
             Text to Speech for Audiobooks
           </div>
-          <div className="font-custom text-2xl sm:text-5xl md:text-6xl lg:text-7xl mx-5 my-4 opacity-90">
+          <div className="font-custom text-2xl sm:text-5xl md:text-6xl lg:text-7xl mx-1 my-3 opacity-90">
             AI-Powered Audiobook Narration
           </div>
           <div className="font-custom opacity-60 text-sm sm:text-base md:text-lg lg:text-lg">
@@ -151,7 +151,7 @@ const SectionTwo = () => {
           </div>
         </div>
 
-        <div className="flex flex-col lg:justify-end mt-6 sm:justify-start sm:mt-10 lg:mt-16 lg:mr-24 mr-8 lg:ml-9 ml-8 shadow-lg shadow-black hover:shadow-indigo-900/40 ">
+        <div className="flex flex-col lg:justify-end mt-6 sm:justify-start sm:mt-10 lg:mt-16 lg:mr-24 mr-1 lg:ml-9 ml-1 shadow-lg shadow-black hover:shadow-indigo-900/40 ">
           <div
             className=" flex w-full px-2 py-2"
           >
